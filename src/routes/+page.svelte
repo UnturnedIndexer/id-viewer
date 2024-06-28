589592615f84bd536fcacbef2f0428ff31dabc27
sovnya <enp7s1@tutanota.com>
2024-06-28 21:13:32 +0200

    Thats a button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
+<script lang="ts">
+    import { Button } from "$lib/components/ui/button";
+</script>
+
 <svelte:head>
     <title>Home</title>
     <meta name="description" content="Svelte demo app" />
 </svelte:head>
 
 <h1>haii</h1>
+
+<Button>Click me</Button>
